09c1ade0bd69aadb0a97a224bfbc1a2930d803bf
ayoubdkhissi <1999dkhissi@gmail.com>
2023-01-23 14:29:13 +0100

    Iterator: Implementation & test

--- Iterator/Implementation.cs
+++ Iterator/Implementation.cs
@@ -1,1 +1,71 @@
 ﻿namespace Iterator;
+
+
+
+public class Person
+{
+    public string Name { get; set; }
+    public String Country { get; set; }
+
+    public Person(string name, string country)
+    {
+        Name = name;
+        Country = country;
+    }
+}
+
+// Iterator interface
+public interface IPeopleIterator
+{
+    Person First();
+    Person Next();
+    bool IsDone { get; }
+    Person Current { get; }
+}
+
+public interface IPeopleCollection
+{
+    IPeopleIterator CreateIterator();
+}
+
+// concrete aggregate
+public class PeopleCollection : List<Person>, IPeopleCollection
+{
+    public IPeopleIterator CreateIterator()
+    {
+        return new PeopleIterator(this);
+    }
+
+}
+
+public class PeopleIterator : IPeopleIterator
+{
+
+    private readonly PeopleCollection _peopleCollection;
+    private int _current = 0;
+    
+    public PeopleIterator(PeopleCollection peopleCollection)
+    {
+        _peopleCollection = peopleCollection;
+    }
+
+    public Person First()
+    {
+        _current = 0;
+        return _peopleCollection
+            .OrderBy(p => p.Name)
+            .ToList()[_current];
+    }
+
+    public bool IsDone => _peopleCollection.Count <= _current;
+
+    public Person Current => _peopleCollection.OrderBy(p => p.Name).ToList()[_current];
+
+    public Person Next()
+    {
+        _current++;
+        if (IsDone)
+            return null;
+        return _peopleCollection.OrderBy(p => p.Name).ToList()[_current];
+    }
+}
--- Iterator/Program.cs
+++ Iterator/Program.cs
@@ -1,3 +1,17 @@
-﻿Console.Title = "Iterator";
+﻿using Iterator;
 
-Console.WriteLine("Hello World!");
+Console.Title = "Iterator";
+
+PeopleCollection people = new PeopleCollection();
+IPeopleIterator iterator = new PeopleIterator(people);
+
+people.Add(new Person("Lachger Abdelmonaim", "Morocco"));
+people.Add(new Person("Dkhissi Ayoub", "Netherlands"));
+people.Add(new Person("Amara Asmae", "Nepal"));
+people.Add(new Person("Boukhallad Aissam", "Mars"));
+
+while (!iterator.IsDone)
+{
+    Console.WriteLine(iterator.Current.Name);
+    iterator.Next();
+}
